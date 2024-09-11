33f0a18c18088510ed122d18e28e0ea921e3f770
Maitri Bhagat <maitri26july@gmail.com>
2024-09-11 15:30:29 +0530

    Initial Commit

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into TODO
+values(10001, 'Learn Spring Boot', false, CURRENT_DATE(), 'in28minutes');
+insert into TODO
+values(10002, 'Learn Angular JS', false, CURRENT_DATE(), 'in28minutes');
+insert into TODO
+values(10003, 'Learn to Dance', false, CURRENT_DATE(), 'in28minutes');
